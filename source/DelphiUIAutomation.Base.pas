e5ccb225cac266265c558f9cc84bb301e3da1d27
windflee <windflee@gmail.com>
2024-11-13 14:11:05 +0800

    Update DelphiUIAutomation.Base.pas

--- source/DelphiUIAutomation.Base.pas
+++ source/DelphiUIAutomation.Base.pas
@@ -20,36 +20,29 @@
 {                                                                           }
 {***************************************************************************}
 unit DelphiUIAutomation.Base;
-
 interface
-
 uses
   types,
   DelphiUIAutomation.Condition,
   UIAutomationClient_TLB;
-
 type
   IAutomationBase = interface
     ['{E858CFB6-C144-444F-9743-5F2F1919EC8D}']
     function GetName: string;
     function GetBoundingRectangle : TRect;
-
     /// <summary>
     ///  Gets the name of the element
     /// </summary>
     property Name : string read getName;
-
     /// <summary>
     ///  Gets the bounding rectangle of the element
     /// </summary>
     property BoundingRectangle : TRect read GetBoundingRectangle;
-
     ///<summary>
     /// Gets the window handle of the Window Element
     ///</summary>
     function GetHandle : Pointer;
   end;
-
   /// <summary>
   ///  The base class for automation objects
   /// </summary>
@@ -71,110 +64,86 @@ type
     function GetTogglePattern: IUIAutomationTogglePattern;
     function GetGridPattern: IUIAutomationGridPattern;
     function GetTablePattern: IUIAutomationTablePattern;
-
     /// <summary>
     ///  Finds the elements
     /// </summary>
     function FindAll : IUIAutomationElementArray; overload;
-
     /// <summary>
     ///  Finds the elements, based on scope
     /// </summary>
     function FindAll (scope : TreeScope) : IUIAutomationElementArray; overload;
-
     /// <summary>
     ///  Finds the elements, based on scope and condition
     /// </summary>
     function FindAll (scope : TreeScope; condition : ICondition) : IUIAutomationElementArray; overload;
-
   public
     /// <summary>
     ///  Gets the name of the element
     /// </summary>
     property Name : string read getName;
-
     /// <summary>
     ///  Gets the bounding rectangle of the element
     /// </summary>
     property BoundingRectangle : TRect read GetBoundingRectangle;
-
     /// <summary>
     ///  Constructor for the element.
     /// </summary>
     constructor Create(element : IUIAutomationElement); virtual;
-
     ///<summary>
     /// Gets the window handle of the Window Element
     ///</summary>
     function GetHandle : Pointer;
+    function GetUIAElement: IUIAutomationElement;
   end;
-
 implementation
-
 uses
   DelphiUIAutomation.PatternIDs,
   DelphiUIAutomation.Exception,
   DelphiUIAutomation.Automation;
-
 constructor TAutomationBase.Create(element: IUIAutomationElement);
 begin
   inherited create;
-
   Felement := element;
 end;
-
 function TAutomationBase.FindAll: IUIAutomationElementArray;
 begin
   result := self.FindAll (TreeScope_Children);
 end;
-
 function TAutomationBase.FindAll(scope: TreeScope): IUIAutomationElementArray;
 var
   condition : ICondition;
   collection : IUIAutomationElementArray;
-
 begin
   condition := TUIAuto.CreateTrueCondition;
-
   // Find the elements
   self.FElement.FindAll(scope, condition.getCondition, collection);
-
   result := collection;
 end;
-
 function TAutomationBase.FindAll(scope: TreeScope;
   condition: ICondition): IUIAutomationElementArray;
 var
   collection : IUIAutomationElementArray;
-
 begin
   // Find the elements
   self.FElement.FindAll(scope, condition.getCondition, collection);
-
   result := collection;
 end;
-
 function TAutomationBase.GetBoundingRectangle: TRect;
 var
   rect : tagRect;
   outRect : TRect;
-
 begin
   Felement.Get_CurrentBoundingRectangle(rect);
-
   outRect.Top := rect.top;
   outRect.Left := rect.left;
   outRect.Width := rect.right;
   outRect.Height := rect.bottom;
-
   result := outRect;
 end;
-
 function TAutomationBase.GetTablePattern : IUIAutomationTablePattern;
 var
   inter: IInterface;
   pattern : IUIAutomationTablePattern;
-
 begin
   fElement.GetCurrentPattern(UIA_TablePatternId, inter);
   if (inter <> nil) then
@@ -184,15 +153,12 @@ begin
       raise EDelphiAutomationException.Create('Unable to initialise control pattern');
     end;
   end;
-
   result := pattern;
 end;
-
 function TAutomationBase.GetGridPattern : IUIAutomationGridPattern;
 var
   inter: IInterface;
   pattern : IUIAutomationGridPattern;
-
 begin
   fElement.GetCurrentPattern(UIA_GridPatternId, inter);
   if (inter <> nil) then
@@ -202,17 +168,13 @@ begin
       raise EDelphiAutomationException.Create('Unable to initialise control pattern');
     end;
   end;
-
   result := pattern;
 end;
-
 function TAutomationBase.GetSelectionItemPattern : IUIAutomationSelectionItemPattern;
 var
   inter: IInterface;
   pattern : IUIAutomationSelectionItemPattern;
-
 begin
-
   fElement.GetCurrentPattern(UIA_SelectionItemPatternId, inter);
   if (inter <> nil) then
   begin
@@ -221,15 +183,12 @@ begin
       raise EDelphiAutomationException.Create('Unable to initialise control pattern');
     end;
   end;
-
   result := pattern;
 end;
-
 function TAutomationBase.GetSelectionPattern: IUIAutomationSelectionPattern;
 var
   inter: IInterface;
   pattern: IUIAutomationSelectionPattern;
-
 begin
   fElement.GetCurrentPattern(UIA_SelectionPatternId, inter);
   if (inter <> nil) then
@@ -239,15 +198,12 @@ begin
       raise EDelphiAutomationException.Create('Unable to initialise control pattern');
     end;
   end;
-
   result := pattern;
 end;
-
 function TAutomationBase.GetValuePattern : IUIAutomationValuePattern;
 var
   inter: IInterface;
   pattern : IUIAutomationValuePattern;
-
 begin
   fElement.GetCurrentPattern(UIA_ValuePatternId, inter);
   if (inter <> nil) then
@@ -257,18 +213,14 @@ begin
       raise EDelphiAutomationException.Create('Unable to initialise value pattern');
     end;
   end;
-
   result := pattern;
 end;
-
 function TAutomationBase.GetExpandCollapsePattern : IUIAutomationExpandCollapsePattern;
 var
   inter: IInterface;
   pattern : IUIAutomationExpandCollapsePattern;
-
 begin
   self.fElement.GetCurrentPattern(UIA_ExpandCollapsePatternId, inter);
-
   if (inter <> nil) then
   begin
     if inter.QueryInterface(IID_IUIAutomationExpandCollapsePattern, pattern) <> S_OK then
@@ -276,18 +228,14 @@ begin
       raise EDelphiAutomationException.Create('Unable to initialise control pattern');
     end;
   end;
-
   result := pattern;
 end;
-
 function TAutomationBase.GetInvokePattern : IUIAutomationInvokePattern;
 var
   unknown: IInterface;
   Pattern  : IUIAutomationInvokePattern;
-
 begin
   fElement.GetCurrentPattern(UIA_InvokePatternID, unknown);
-
   if (unknown <> nil) then
   begin
     if unknown.QueryInterface(IUIAutomationInvokePattern, Pattern) <> S_OK then
@@ -295,18 +243,14 @@ begin
       raise EDelphiAutomationException.Create('Unable to initialise control pattern');
     end;
   end;
-
   result := pattern;
 end;
-
 function TAutomationBase.GetWindowPattern : IUIAutomationWindowPattern;
 var
   inter: IInterface;
   pattern: IUIAutomationWindowPattern;
-
 begin
   self.fElement.GetCurrentPattern(UIA_WindowPatternId, inter);
-
   if (inter <> nil) then
   begin
     if inter.QueryInterface(IID_IUIAutomationWindowPattern, pattern) <> S_OK then
@@ -314,18 +258,14 @@ begin
       raise EDelphiAutomationException.Create('Unable to initialise Window control pattern');
     end;
   end;
-
   result := pattern;
 end;
-
 function TAutomationBase.GetTogglePattern: IUIAutomationTogglePattern;
 var
   inter: IInterface;
   pattern : IUIAutomationTogglePattern;
-
 begin
   self.fElement.GetCurrentPattern(UIA_TogglePatternId, inter);
-
   if (inter <> nil) then
   begin
     if inter.QueryInterface(IID_IUIAutomationTogglePattern, pattern) <> S_OK then
@@ -333,26 +273,25 @@ begin
       raise EDelphiAutomationException.Create('Unable to initialise control pattern');
     end;
   end;
-
   result := pattern;
+end;
+function TAutomationBase.GetUIAElement: IUIAutomationElement;
+begin
+  Result := FElement;
 end;
 
 function TAutomationBase.getName: string;
 var
   name : widestring;
-
 begin
   FElement.Get_CurrentName(name);
   result := name;
 end;
-
 function TAutomationBase.GetHandle: Pointer;
 var
   retVal : Pointer;
 begin
   self.FElement.Get_CurrentNativeWindowHandle(retVal);
-
   result := retVal;
 end;
-
 end.
